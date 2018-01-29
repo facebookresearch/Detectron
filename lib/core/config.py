a6b135ed777a6fb69dd64d804c97fb10cc2dec42
Galen Han <Mastergalen@users.noreply.github.com>
2018-01-29 13:13:24 +0000

    Change download cache location to be persistent

--- lib/core/config.py
+++ lib/core/config.py
@@ -955,8 +955,7 @@ __C.EXPECTED_RESULTS_EMAIL = b''
 
 # Models and proposals referred to by URL are downloaded to a local cache
 # specified by DOWNLOAD_CACHE
-__C.DOWNLOAD_CACHE = b'/tmp/detectron-download-cache'
-
+__C.DOWNLOAD_CACHE = b'~/detectron-download-cache'
 
 # ---------------------------------------------------------------------------- #
 # Cluster options
