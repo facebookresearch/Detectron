c3fefcbed5edd06a469fe0a6ba67d97d0482c113
Jon Crall <jon.crall@kitware.com>
2018-03-09 11:16:30 -0500

    specific changes round4

--- tools/convert_pkl_to_pb.py
+++ tools/convert_pkl_to_pb.py
@@ -100,7 +100,7 @@ def parse_args():
         default=1,
         type=int)
     parser.add_argument(
-        'opts', help='See lib/core/config.py for all options', default=None,
+        'opts', help='See detectron/core/config.py for all options', default=None,
         nargs=argparse.REMAINDER)
     if len(sys.argv) == 1:
         parser.print_help()
