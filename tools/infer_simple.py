2a7bd5549347a43065c9eee2483553342f7fd00e
Gioele Ciaparrone <Juggernaut93@users.noreply.github.com>
2018-04-27 15:32:21 +0200

    Added input argument for images with no boxes

--- tools/infer_simple.py
+++ tools/infer_simple.py
@@ -83,6 +83,12 @@ def parse_args():
         type=str
     )
     parser.add_argument(
+        '--always-out',
+        dest='out_when_no_box',
+        help='output image even when no object is found',
+        action='store_true'
+    )
+    parser.add_argument(
         'im_or_folder', help='image or folder of images', default=None
     )
     if len(sys.argv) == 1:
@@ -137,7 +143,8 @@ def main(args):
             box_alpha=0.3,
             show_class=True,
             thresh=0.7,
-            kp_thresh=2
+            kp_thresh=2,
+            out_when_no_box=args.out_when_no_box
         )
 
 
