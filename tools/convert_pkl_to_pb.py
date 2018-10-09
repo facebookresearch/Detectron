2cd264d61e00f9dfc2d3a9ec9c0c56ec64454cb1
zhangnd <zhangnaidong@seemmo.com>
2018-10-09 11:03:35 +0800

    add convert op BatchPermutation to Gather

--- tools/convert_pkl_to_pb.py
+++ tools/convert_pkl_to_pb.py
@@ -268,6 +268,14 @@ def convert_net(args, net, blobs):
                                                 height_scale=float(scale))
         return resize_nearest_op
 
+    @op_filter(type='BatchPermutation')
+    def convert_batch_permutation(op):
+        gather_op = core.CreateOperator('Gather',
+                                        list(op.input),
+                                        list(op.output),
+                                        name=op.name)
+        return gather_op
+
     @op_filter()
     def convert_rpn_rois(op):
         for j in range(len(op.input)):
@@ -291,6 +299,7 @@ def convert_net(args, net, blobs):
     # so run separately
     convert_op_in_proto(net, convert_remove_op)
     convert_op_in_proto(net, convert_upsample_nearest)
+    convert_op_in_proto(net, convert_batch_permutation)
     convert_op_in_proto(net, convert_python)
     convert_op_in_proto(net, convert_op_name)
     convert_op_in_proto(net, convert_rpn_rois)
