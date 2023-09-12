2574268d50140332dd0f8d494dc1aeed28718f55
Igor Sugak <sugak@meta.com>
2023-09-12 16:50:12 -0700

    s,np.bool\b,bool,
    Summary: `np.bool` and `bool` are identical, but the former is removed from Numpy-1.24.X.
    
    Differential Revision: D49196618

--- detectron/datasets/voc_eval.py
+++ detectron/datasets/voc_eval.py
@@ -148,7 +148,7 @@ def voc_eval(detpath,
     for imagename in imagenames:
         R = [obj for obj in recs[imagename] if obj['name'] == classname]
         bbox = np.array([x['bbox'] for x in R])
-        difficult = np.array([x['difficult'] for x in R]).astype(np.bool)
+        difficult = np.array([x['difficult'] for x in R]).astype(bool)
         det = [False] * len(R)
         npos = npos + sum(~difficult)
         class_recs[imagename] = {'bbox': bbox,
